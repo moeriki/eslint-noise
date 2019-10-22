14dafa1df3ab6e401879383a74c4ae7d2973394c
Dieter Luypaert <dieterluypaert@gmail.com>
2019-10-22 16:44:44 +0200

    Feat: Dog food config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,4 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  env: { es6: true, node: true },
-  parserOptions: { ecmaVersion: 2020 },
+  extends: ['noise-node'],
   root: true,
-  rules: { 'prettier/prettier': 'error' },
 };
