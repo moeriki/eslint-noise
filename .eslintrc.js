03362527d7cdd8c4afc0da55f83517ad9b274111
Dieter Luypaert <dieterluypaert@gmail.com>
2019-10-28 13:12:24 +0100

    Chore: Improve readability

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
 const { extendRule } = require('./packages/eslint-config-noise-base/utils');
-const {
-  rules,
-} = require('./packages/eslint-config-noise-base/rules/plugin-import');
+const pluginImport = require('./packages/eslint-config-noise-base/rules/plugin-import');
 
 module.exports = {
   extends: ['noise-node'],
@@ -14,7 +12,7 @@ module.exports = {
   root: true,
   rules: {
     'import/no-internal-modules': extendRule(
-      rules['import/no-internal-modules'],
+      pluginImport.rules['import/no-internal-modules'],
       { allow: ['eslint-config-noise-*/**'] },
     ),
     'no-magic-numbers': 'off',
