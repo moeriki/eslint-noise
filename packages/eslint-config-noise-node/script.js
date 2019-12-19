778c545e0ff884362ad22d3f734aef7c6f61a370
Dieter Luypaert <dieterluypaert@gmail.com>
2019-12-19 10:13:14 +0100

    fix: loosen scripts

--- packages/eslint-config-noise-node/script.js
+++ packages/eslint-config-noise-node/script.js
@@ -3,6 +3,8 @@
 module.exports = {
   rules: {
     'no-console': 'off',
+    'promise/always-return': 'off',
+    'node/no-unpublished-import': 'off',
     'node/no-unpublished-require': 'off',
     'node/shebang': 'off',
   },
