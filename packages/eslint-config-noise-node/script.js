20a4a77045a5c73b2fc2e9f6293f5725839f441c
Dieter Luypaert <dieterluypaert@gmail.com>
2020-02-26 09:52:06 +0100

    fix: allow anonymous default export in scripts

--- packages/eslint-config-noise-node/script.js
+++ packages/eslint-config-noise-node/script.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'promise/always-return': 'off',
+    'import/no-anonymous-default-export': 'off',
     'node/no-unpublished-import': 'off',
     'node/no-unpublished-require': 'off',
     'node/shebang': 'off',
