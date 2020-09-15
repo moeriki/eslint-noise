0f6630ee1fe8a36e336deff2e86d0dcea004655c
Dieter Luypaert <dieterluypaert@gmail.com>
2020-09-15 09:42:24 +0200

    feat(ts): bump ts-eslint to v3.10

--- packages/eslint-config-noise-ts/rules/plugin-typescript-eslint.js
+++ packages/eslint-config-noise-ts/rules/plugin-typescript-eslint.js
@@ -122,6 +122,9 @@ module.exports = {
       { allowWithDecorator: true },
     ],
 
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-implicit-any-catch.md
+    '@typescript-eslint/no-implicit-any-catch': 'error',
+
     // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-inferrable-types.md
     '@typescript-eslint/no-inferrable-types': 'error',
 
