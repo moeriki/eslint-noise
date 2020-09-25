fe30c0559f4a3f2536ec8cf8c0926591edd45c3d
Dieter Luypaert <dieterluypaert@gmail.com>
2020-09-25 14:03:16 +0200

    feat: allow internal imports for next/document

--- packages/eslint-config-noise/rules/plugin-import.js
+++ packages/eslint-config-noise/rules/plugin-import.js
@@ -120,7 +120,7 @@ module.exports = {
           'eslint-config-*/rules',
           'firebase/{analytics,app,auth,database,firestore}',
           'lodash/**',
-          'next/{amp,app,head,link,router}',
+          'next/{amp,app,document,head,link,router}',
           'uuid/v?',
         ],
       },
