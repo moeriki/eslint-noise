a9c3e2a12da3feb6e3bf8dd66abbceea002840ab
Dieter Luypaert <dieterluypaert@gmail.com>
2021-11-30 08:18:02 +0100

    🐛 include extra file extension for vue

--- packages/eslint-config-noise/vue/ts.js
+++ packages/eslint-config-noise/vue/ts.js
@@ -3,6 +3,9 @@
 module.exports = {
   parserOptions: {
     parser: '@typescript-eslint/parser',
+    parserOptions: {
+      extraFileExtensions: ['.vue'],
+    },
   },
   rules: {
     'no-undef': 'off',
