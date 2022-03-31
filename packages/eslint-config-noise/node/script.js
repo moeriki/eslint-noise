48a06b7bf2ed4553bd0115024c991c7487fbd603
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-31 13:22:00 +0200

    🎨 group by type

--- packages/eslint-config-noise/node/script.js
+++ packages/eslint-config-noise/node/script.js
@@ -4,11 +4,16 @@ module.exports = {
   rules: {
     'no-console': 'off',
     strict: 'off',
-    'promise/always-return': 'off',
-    'promise/no-nesting': 'off',
+
     'import/no-anonymous-default-export': 'off',
+
     'node/no-unpublished-import': 'off',
     'node/no-unpublished-require': 'off',
     'node/shebang': 'off',
+
+    'promise/always-return': 'off',
+    'promise/no-nesting': 'off',
+
+    'unicorn/no-process-exit': 'error',
   },
 };
